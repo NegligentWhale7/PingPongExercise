f0fee75572f2457b49ae40f89c227f6cf0ab9307
Alam Armas <carlosalam33@hotmail.com>
2025-07-21 14:28:04 -0600

    Init

---
+++ src/main/java/com/josealam/pingpongexercise/PingpongexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.josealam.pingpongexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpongexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpongexerciseApplication.class, args);
+	}
+
+}
