9dfa3cf13aeb1c89de70e1abfe2cd8a16ce1fb93
JoseSolano-20 <josemanuel.solano@solera.com>
2025-07-21 16:20:54 -0600

    add relations

--- src/main/java/com/josealam/pingpongexercise/models/Vehicle.java
+++ src/main/java/com/josealam/pingpongexercise/models/Vehicle.java
@@ -1,9 +1,15 @@
 package com.josealam.pingpongexercise.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,4 +25,15 @@ public class Vehicle {
     private String anio;
     private String color;
     private String vin;
+
+    @ManyToOne
+    @JoinColumn(name = "workshop_id", nullable = false)
+    private Workshop workshop;
+
+    @ManyToMany
+    @JoinTable(
+        name = "vehicle_parts",
+        joinColumns = @JoinColumn(name = "vehicle_id"),
+        inverseJoinColumns = @JoinColumn(name = "part_id"))
+        private List<Part> parts;
 }
